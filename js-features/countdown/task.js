04846c6d15f5e7ee3e0be5772636ef243d929ef1
Bogdan Novikov <fikis57@mail.ru>
2019-07-07 23:01:32 +0300

    add first commit

---
+++ js-features/countdown/task.js
@@ -0,0 +1,11 @@
+const counter = function() {
+    const timer = document.getElementById("timer");
+    if (timer.textContent == 0) {
+        alert("Вы победили в конкурсе!");
+        clearInterval(myinterval);
+
+    } else {
+        timer.textContent -= 1;
+    }
+}    
+let myinterval = setInterval(counter, 1000);
